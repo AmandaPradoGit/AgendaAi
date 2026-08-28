78966d5d7ea0d9447f5a6b99787a24ddef03fa34
AmandaPradoGit <amandapradogc@gmail.com>
2026-08-28 10:30:10 -0300

    fix: correct table names in migration files for clientes, produtos, and itens_pedido

--- database/migrations/2026_08_25_130102_create_itens_pedidos.php
+++ database/migrations/2026_08_25_130102_create_itens_pedidos.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('itens_pedidos');
+        Schema::dropIfExists('itens_pedido');
     }
 };
